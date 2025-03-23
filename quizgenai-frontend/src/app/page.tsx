ccc67f70e199ff07a922f75febb34a68d31b40df
Aaquib <aaquibskimbox@gmail.com>
2025-03-22 23:09:26 -0400

    added few css to landing page

--- quizgenai-frontend/src/app/page.tsx
+++ quizgenai-frontend/src/app/page.tsx
@@ -1,5 +1,22 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">This is Landing page</h1>;
+  return (
+    <div className="h-screen bg-hero-custom flex justify-center items-center">
+      <div className="app-description text-center max-w-200 w-full">
+        <h1 className="text-7xl font-bold text-center mb-[16px]">
+          From idea to scale.
+          <br />
+          Simplified.
+        </h1>
+        <div className="text-2xl font-light text-center max-w-400 mb-[32px]">
+          Ship production apps at lightning speed, and scale to a global
+          audience effortlessly with our next-gen serverless database.
+        </div>
+        <button className="px-[24px] py-[12px] bg-(--primary) rounded-(--borderRadius) font-bold text-1xl">
+          Get Started Now
+        </button>
+      </div>
+    </div>
+  );
 }
